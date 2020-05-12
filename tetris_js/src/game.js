3f29350e9fbd5fdc135b95ae4a839e0379992f14
aleksey-13 <alekseymagadan@gmail.com>
2020-05-12 17:19:40 +0300

    tetris: add methods for figure movement

--- tetris_js/src/game.js
+++ tetris_js/src/game.js
@@ -26,11 +26,65 @@ export default class Game {
   ];
   activePiece = {
     x: 0,
-    y: 0,
+    y: 13,
     blocks: [
       [0, 1, 0],
       [1, 1, 1],
       [0, 0, 0],
     ],
   };
+
+  movePieceLeft() {
+    this.activePiece.x -= 1;
+
+    if (this.hasCollision()) {
+      this.activePiece.x += 1;
+    }
+  }
+
+  movePieceRight() {
+    this.activePiece.x += 1;
+
+    if (this.hasCollision()) {
+      this.activePiece.x -= 1;
+    }
+  }
+
+  movePieceDown() {
+    this.activePiece.y += 1;
+
+    if (this.hasCollision()) {
+      this.activePiece.y -= 1;
+      this.lockPiece();
+    }
+  }
+
+  hasCollision() {
+    const { x: pieceX, y: pieceY, blocks } = this.activePiece;
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (
+          blocks[y][x] &&
+          (this.playField[pieceY + y] === undefined ||
+            this.playField[pieceY + y][pieceX + x] === undefined ||
+            this.playField[pieceY + y][pieceX + x])
+        ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  lockPiece() {
+    const { x: pieceX, y: pieceY, blocks } = this.activePiece;
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          this.playField[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+  }
 }
